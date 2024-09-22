d5d09127431a1640c352911503078fc9e25c4c70
Rick <rick@rickdot.net>
2024-09-22 12:50:04 -0500

    add nats

--- src/DasMonitor/Program.cs
+++ src/DasMonitor/Program.cs
@@ -1,29 +1,9 @@
-﻿using Apollo.Extensions.Microsoft.Hosting;
-using DasMonitor;
-using DasMonitor.Abstractions;
-using Microsoft.Extensions.DependencyInjection;
+﻿using DasMonitor;
 using Microsoft.Extensions.Hosting;
 
-var builder = Host.CreateApplicationBuilder();
+var host = Host
+    .CreateApplicationBuilder()
+    .ConfigureHost();
 
-// keyboard client
-builder.Services.AddHttpClient<DasSignalClient>(
-    // only necessary if you need to override the base address
-    // this is the default base address if you don't specify one
-    //client => client.BaseAddress = new Uri(DasMonitorConstants.BaseSignalUrl)
-);
-
-// apollo plumbing
-builder.Services.AddApollo(ab =>
-    ab.AddEndpoint<DasMonitorEndpoint>(DasMonitorConstants.EndpointConfig)
-);
-
-// listening
-builder.Services.AddSingleton<DasMonitorEndpoint>();
-
-// publishing (HostDemo)
-builder.Services.AddSingleton<DasPublisher>();
-
-var host = builder.Build();
-//host.Run();  // this won't do anything yet, it's in-memory only
-await DasDemo.HostDemo(host);
+host.Run();
+//await DasDemo.HostDemo(host);
